94674d51f2e9a348dccb144aada9f7bfe3b4a501
Aaron Lau <aaron.lau@datadoghq.com>
2019-09-30 22:31:15 -0400

    using stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_Fq1no0WjFXAE6e1pR3hkkLQ5'
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful')
+    }
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Re Fresh Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            alipay
+            bitcoin
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
